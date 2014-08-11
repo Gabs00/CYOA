8c8d0772bfb5d764ed887862072053ba11ecd350
Gabs00 <gewen87@gmail.com>
2014-08-11 05:05:23 -0400

    force socket.io to use long polling

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ var connect = require('connect'),
   st = require('serve-static'),
   backboneio = require('backbone.io');
 
+var io = require('socket.io');
+
+io.configure( function() {
+  io.set("transports", ["xhr-polling"]);
+  io.set("polling duration",10);
+});
 
 var app = connect()
   .use(st(__dirname + '/public'))
