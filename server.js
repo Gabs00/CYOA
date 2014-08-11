3a3f22c8546d9965b3930d0a2ea8ea913c9231da
Gabs00 <gewen87@gmail.com>
2014-08-11 05:08:21 -0400

    removed socket.io

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
 var connect = require('connect'),
   st = require('serve-static'),
   backboneio = require('backbone.io');
-
-var io = require('socket.io');
-
-io.configure( function() {
-  io.set("transports", ["xhr-polling"]);
-  io.set("polling duration",10);
-});
-
+  
 var app = connect()
   .use(st(__dirname + '/public'))
   .listen(process.env.PORT || 3000);
