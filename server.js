f6bfa08bbc386028818b80cc78ab530522b45d61
Gabs00 <gewen87@gmail.com>
2014-08-11 04:14:11 -0400

    added backbone.io

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 var connect = require('connect'),
-  st = require('serve-static');
+  st = require('serve-static'),
+  backboneio = require('backbone.io');
+
+
 var app = connect()
   .use(st(__dirname + '/public'))
   .listen(3000);
+
+var backend = backboneio.createBackend();
+backend.use(backboneio.middleware.memoryStore());
+var io = backboneio.listen(app, {questionBackend: backend});
